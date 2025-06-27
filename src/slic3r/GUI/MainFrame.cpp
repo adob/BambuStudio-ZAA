027a4e2d7d9ed14262213ba3b8c63ae0a4b1c541
Aleksandr Dobkin <alex@dobk.in>
2025-06-27 02:18:09 -0700

    add nonplanar files

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -1,5 +1,9 @@
 #include "MainFrame.hpp"
 #include "GLToolbar.hpp"
+
+#include <boost/filesystem/directory.hpp>
+#include <boost/filesystem/operations.hpp>
+
 #include <wx/panel.h>
 #include <wx/notebook.h>
 #include <wx/listbook.h>
@@ -28,6 +32,7 @@
 #include "ParamsDialog.hpp"
 #include "UserPresetsDialog.hpp"
 #include "PrintHostDialogs.hpp"
+#include "libslic3r/Utils.hpp"
 #include "wxExtensions.hpp"
 #include "GUI_ObjectList.hpp"
 #include "Mouse3DController.hpp"
@@ -2639,6 +2644,16 @@ void MainFrame::init_menubar_as_editor()
                 open_recent_project(file_id, filename);
             }, wxID_FILE1, wxID_FILE1 + 49); // [5050, 5100)
 
+        std::vector<std::string> non_planar_projects;
+        for (auto &&entry : fs::directory_iterator(resources_dir() + "/nonplanar")) {
+            if (fs::is_regular_file(entry) && entry.path().extension() == ".3mf") {
+                non_planar_projects.push_back(entry.path().string());
+            }
+        }
+        std::sort(non_planar_projects.begin(), non_planar_projects.end());
+        for (auto &&path : non_planar_projects) {
+            m_recent_projects.AddFileToHistory(from_u8(path));
+        }
         std::vector<std::string> recent_projects = wxGetApp().app_config->get_recent_projects();
         std::reverse(recent_projects.begin(), recent_projects.end());
         for (const std::string& project : recent_projects)
